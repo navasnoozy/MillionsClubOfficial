8e93ea7bb3d0b40f268dccbb9bb9878a421660e1
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-06-15 20:44:31 +0530

    proper credetial error

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -5,43 +5,44 @@ import { comparePassword } from "../utils/hashPassword";
 import { User } from "../models/userModel";
 import jwt from "jsonwebtoken";
 import { validateRequest } from "../middlewares/requestValidation";
+import { BadRequestError } from "../errors/Bad-requestError";
 
 const router = express.Router();
 
-router.get("/api/users/signin",validateRequest(signinSchema), async (req, res) => {
-  const body = req.body;
+router.get(
+  "/api/users/signin",
+  validateRequest(signinSchema),
+  async (req, res) => {
+    const body = req.body;
 
-  const { email, password } = body;
-  const user = await User.findOne({ email });
+    const { email, password } = body;
+    const user = await User.findOne({ email });
 
-  if (!user || !user.email) {
-    res.status(404).send("");
-    return;
+    if (!user || !user.email) {
+      throw new BadRequestError("Invalid Email id or password");
+    }
+
+    const isPasswordMatch = await comparePassword(password, user.password);
+    if (!isPasswordMatch) {
+      throw new BadRequestError("Invalid Email id or password");
+    }
+
+    if (isPasswordMatch) {
+      const jwt_token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+        },
+        process.env.JWT_KEY!
+      );
+
+      req.session = {
+        jwt: jwt_token,
+      };
+    }
+
+    res.status(200).send(user);
   }
-
-  const isPasswordMatch = await comparePassword(password, user.password);
-  if (!isPasswordMatch) {
-    res
-      .status(404)
-      .send({ success: false, error: "Invalid email id or password" });
-    return;
-  }
-
-  if (isPasswordMatch) {
-    const jwt_token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-      },
-      process.env.JWT_KEY!
-    );
-
-    req.session = {
-      jwt: jwt_token,
-    };
-  }
-
-  res.send(user);
-});
+);
 
 export { router as signin };
