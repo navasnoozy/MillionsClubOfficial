d0d776a2c88b7e7794f12133cc78554a54424ce8
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-06-17 20:18:25 +0530

    split index.ts to index.ts and app.ts

---
+++ auth/src/app.ts
@@ -0,0 +1,38 @@
+import cookieSession from "cookie-session";
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./config/db";
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/errorHandler";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { currentUserRouter } from "./routes/current-user";
+
+const app = express();
+
+app.use(express.json());
+dotenv.config();
+
+app.set("trust proxy", true);
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(currentUserRouter);
+
+// Invalid route error
+app.all("*path", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,41 +1,7 @@
-import cookieSession from "cookie-session";
-import dotenv from "dotenv";
-import express from "express";
+import { app } from "./app";
 import connectDB from "./config/db";
-import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middlewares/errorHandler";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { currentUserRouter } from "./routes/current-user";
-
-const app = express();
 
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-dotenv.config();
-
-app.set("trust proxy", true);
-
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(currentUserRouter);
-
-// Invalid route error
-app.all("*path", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("server running on", port);
